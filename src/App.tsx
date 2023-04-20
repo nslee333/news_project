b25f716207ac128e93944813334a3b05071a7af2
Nathan <nslee333@gmail.com>
2023-04-20 07:51:02 -0700

    Framework for application.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    
+      <div className="header">
+        <div className="header-inner"></div>
+
+      </div>
+      <div className="main">
+        <div className="main-inner">
+
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="footer-inner">
+
+        </div>
+      </div>
       
     </div>
   );
