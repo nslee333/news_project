8624552960a3e145fa50c043f42b7d134379ce79
Nathan <nslee333@gmail.com>
2023-04-20 21:26:29 -0700

    Formatting and setup for mock data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
-import './css/App.css';
-import './css/header.css';
-import './css/footer.css';
-import './css/main.css';
+import "./css/App.css";
+import "./css/header.css";
+import "./css/footer.css";
+import "./css/main.css";
 
-import { useState } from 'react';
+import { posts } from "./mockData";
+import { useState } from "react";
+
+function newsPosts() {
+  const displayPosts = posts.map((item, index) => {
+    <a href={posts[index].url}>
+      <div className="post">
+        <img
+          className="post-img"
+          src={posts[index].urlToImage}
+          alt="post photo"
+        />
+        <div className="post-title">{posts[index].title}</div>
+      </div>
+    </a>;
+  });
+
+  return <>{displayPosts}</>;
+}
 
 function App() {
   const [prefClick, setPrefClick] = useState(false);
 
-
   return (
     <div className="App">
-    
       <div className="header">
         <div className="header-inner">
-          <div className="header-title">
-            Latest News - Feed
-          </div>
+          <div className="header-title">Latest News - Feed</div>
           <a onClick={() => setPrefClick(!prefClick)}>
-            <div className="header-settings">
-              Settings
-            </div>
+            <div className="header-settings">Settings</div>
           </a>
-
         </div>
       </div>
 
       <div className="main">
-      
-        {(prefClick ? (
+        {prefClick ? (
           <div className="pref-inner">pref</div>
-          ) : (
-          <div className="main-inner">
-          main
-          </div>
-        ))}
+        ) : (
+          <div className="main-inner">{newsPosts()}</div>
+        )}
       </div>
 
       <div className="footer">
-        <div className="footer-inner">
-
-        </div>
+        <div className="footer-inner"></div>
       </div>
-      
     </div>
   );
 }
