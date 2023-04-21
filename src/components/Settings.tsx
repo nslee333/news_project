d7568701279e9ceec769fc0ab5ba1f41a7ea697d
Nathan <nslee333@gmail.com>
2023-04-21 15:08:22 -0700

    WIP: Frame for Settings page.

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,7 +1,34 @@
+import "../css/settings.css";
+
+
+
 export default function Settings() {
+  // & Redux entry here.
+
+
   return (
     <>
-      Hello World
-    </>
+      <div className="sett">
+        <div className="sett-title">
+          Settings
+        </div>
+        <div className="sett-feed">
+          <div className="feed-title">Customize your feed</div>
+          <div className="sett-filter">
+            <div className="filter-title">Filter all but 5 news sources</div>
+            <div className="filter-title">*Search box that contains the sources wanted</div>
+            
+          </div>
+
+          <div className="sett-block">
+            <div className="block-title">Blocked news sources</div>
+            <div className="block-title">Search that contains the blocked news sources.</div>
+        
+          </div>
+        </div>
+
+      </div>
+
+    </> 
   );
 }
