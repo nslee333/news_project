516613f407452af315fd3f8e36587446133584f0
Nathan <nslee333@gmail.com>
2023-04-21 11:00:30 -0700

    WIP: Added mock image, more work on displaying posts.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./css/App.css";
 import "./css/header.css";
 import "./css/footer.css";
 import "./css/main.css";
+import image from "./sample_image.jpg"
 
 import { posts } from "./mockData";
 import { useState } from "react";
@@ -9,16 +10,21 @@ import { useState } from "react";
 function newsPosts() {
   const displayPosts = posts.map((item, index) => {
     return (
-      <a href={posts[index].url}>
-        <div className="post">
+      <div className="post" key={posts[index].id}>
+        <a href={posts[index].url} className="post-link">
           <img
             className="post-img"
-            src={posts[index].urlToImage}
+            // src={posts[index].urlToImage}
+            src={image}
             alt="post photo"
             />
-          <div className="post-title">{posts[index].title}</div>
-        </div>
-      </a>
+          <div className="post-info">
+            <div className="post-title">{posts[index].title}</div>
+            <div className="post-author">{posts[index].author}</div>
+            <div className="post-source">{posts[index].source}</div>
+          </div>
+        </a>
+      </div>
     );
   });
 
