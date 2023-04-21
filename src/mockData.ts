4b8f00ac518fc0a9164fe4692da53db508552164
Nathan <nslee333@gmail.com>
2023-04-21 08:26:57 -0700

    Explicitly typed mock data.

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,44 +1,51 @@
-export const posts = [
-  {
-    source: "website.com",
-    author: "John Doe",
-    title: "Elon musk's wealth drops by learly 13 billion.",
-    url: "url.com",
-    urlToImage: "url.image.com",
-    publishedAt: "2023-04-21T03:34:49Z"
-  },
-  {
-    source: "website.com",
-    author: "John Doe",
-    title: "Elon musk's wealth drops by learly 13 billion.",
-    url: "url.com",
-    urlToImage: "url.image.com",
-    publishedAt: "2023-04-21T03:34:49Z"
-  },
-  {
-    source: "website.com",
-    author: "John Doe",
-    title: "Elon musk's wealth drops by learly 13 billion.",
-    url: "url.com",
-    urlToImage: "url.image.com",
-    publishedAt: "2023-04-21T03:34:49Z"
-  },
-  {
-    source: "website.com",
-    author: "John Doe",
-    title: "Elon musk's wealth drops by learly 13 billion.",
-    url: "url.com",
-    urlToImage: "url.image.com",
-    publishedAt: "2023-04-21T03:34:49Z"
-  },
-  {
-    source: "website.com",
-    author: "John Doe",
-    title: "Elon musk's wealth drops by learly 13 billion.",
-    url: "url.com",
-    urlToImage: "url.image.com",
-    publishedAt: "2023-04-21T03:34:49Z"
-  }
+type Post = {
+  source: string;
+  author: string;
+  title: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+};
 
-]
-
+export const posts: Post[] = [
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z",
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z",
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z",
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z",
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z",
+  },
+];
