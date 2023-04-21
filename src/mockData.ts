04eb4d5630bc18f786c2931e14593ef207595bf1
Nathan <nslee333@gmail.com>
2023-04-20 21:13:27 -0700

    Added mock data.

---
+++ src/mockData.ts
@@ -0,0 +1,44 @@
+export const posts = [
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z"
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z"
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z"
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z"
+  },
+  {
+    source: "website.com",
+    author: "John Doe",
+    title: "Elon musk's wealth drops by learly 13 billion.",
+    url: "url.com",
+    urlToImage: "url.image.com",
+    publishedAt: "2023-04-21T03:34:49Z"
+  }
+
+]
+
