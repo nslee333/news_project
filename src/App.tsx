483c7d3055bfd48019e0428c9b93cfe4f6715056
Nathan <nslee333@gmail.com>
2023-04-21 14:01:07 -0700

    Extracted Posts and Settings components from App to seperate files.

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,12 @@ import "./css/header.css";
 import "./css/footer.css";
 import "./css/main.css";
 import "./css/post.css";
-import image from "./sample_image.jpg";
+import "./css/settings.css"
 
-import { posts } from "./mockData";
+import Posts from "./components/Posts";
+import Settings from "./components/Settings";
 import { useState } from "react";
 
-function Posts() {
-  const displayPosts = posts.map((item, index) => {
-    return (
-      <div className="post" key={posts[index].id}>
-        <a href={posts[index].url} className="post-link">
-          <img
-            className="post-img"
-            // src={posts[index].urlToImage}
-            src={image}
-            alt="post photo"
-          />
-          <div className="post-info">
-            <div className="post-title">{posts[index].title}</div>
-            <div className="post-author">{posts[index].author}</div>
-            <div className="post-source">{posts[index].source}</div>
-          </div>
-        </a>
-      </div>
-    );
-  });
-
-  return <>{displayPosts}</>;
-}
 
 function App() {
   const [settClick, setSettClick] = useState(false);
@@ -48,7 +26,9 @@ function App() {
 
       <div className="main">
         {!settClick ? (
-          <div className="sett-inner">settings</div>
+          <div className="sett-inner">
+            {Settings()}
+          </div>
         ) : (
           <div className="main-inner">{Posts()}</div>
         )}
---
+++ src/components/Posts.tsx
@@ -0,0 +1,28 @@
+import { posts } from "../mockData";
+import image from "../sample_image.jpg";
+
+
+
+export default function Posts() {
+  const displayPosts = posts.map((item, index) => {
+    return (
+      <div className="post" key={posts[index].id}>
+        <a href={posts[index].url} className="post-link">
+          <img
+            className="post-img"
+            // src={posts[index].urlToImage}
+            src={image}
+            alt="post photo"
+          />
+          <div className="post-info">
+            <div className="post-title">{posts[index].title}</div>
+            <div className="post-author">{posts[index].author}</div>
+            <div className="post-source">{posts[index].source}</div>
+          </div>
+        </a>
+      </div>
+    );
+  });
+
+  return <>{displayPosts}</>;
+}
