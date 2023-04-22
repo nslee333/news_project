df822b057181c35c9ed21316ad97e1dfd984f8e9
Nathan <nslee333@gmail.com>
2023-04-21 19:22:12 -0700

    WIP: Adding redux to project.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from "./reducers";
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
