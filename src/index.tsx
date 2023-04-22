cf8567b5036ac81e7c9989f52c4187c9b2f1028f
Nathan <nslee333@gmail.com>
2023-04-22 09:08:02 -0700

    Switching from Redux to useReducer.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from "./reducers";
-import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
