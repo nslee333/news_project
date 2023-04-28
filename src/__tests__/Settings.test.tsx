ebcf457a6569aa67af71eb5c97c74ba00b7b80d5
Nathan <nslee333@gmail.com>
2023-04-27 18:52:38 -0700

    Added Settings smoke screen test.

---
+++ src/__tests__/Settings.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import Settings from "../components/Settings";
+
+test("Settings should render without rendering", () => {
+  render(<Settings />)
+});
