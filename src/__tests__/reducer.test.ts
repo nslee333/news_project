4483b804ba4c498a3981b5143481f66a1f550359
Nathan <nslee333@gmail.com>
2023-05-03 17:20:44 -0700

    Added tests for headerReducer.

--- src/__tests__/reducer.test.ts
+++ src/__tests__/reducer.test.ts
@@ -21,7 +21,7 @@ test("Settings reducer correctly adds to wanted sources", () => {
   expect(state).toEqual(expectedState);
 });
 
-test("Settings reducer correctly removes from source", () => {
+test("Settings reducer correctly removes from wanted sources", () => {
   const addAction = {
     type: "add_to_sources_wanted",
     source: "wall street",
@@ -72,3 +72,34 @@ test("Settings reducer removes from blocked sources", () => {
 
   expect(stateAfterRemove).toEqual(initialState);
 });
+
+test("headerReducer returns false if switch_from_feed", () => {
+  const initialState = {
+    displaySettings: false,
+  }
+  
+  
+  const settSwitch = {
+    action: "switch_from_feed"
+  }
+  
+  const state = headerReducer(initialState, settSwitch);
+
+  if(state instanceof Error) return;
+  expect(state.displaySettings).toBe(false);
+});
+
+test("headerReducer returns true if switch_from_settings", () => {
+  const initialState = {
+    displaySettings: false,
+  }
+
+  const settSwitch = {
+    action: "switch_from_settings"
+  }
+
+  const state = headerReducer(initialState, settSwitch);
+
+  if (state instanceof Error) return;
+  expect(state.displaySettings).toBe(true);
+})
