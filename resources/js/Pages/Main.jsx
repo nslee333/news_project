9cfae1f2553adf4a904aa5d16636bd4604b479df
Nathan <nslee333@gmail.com>
2023-05-09 18:45:59 -0700

    Migrating main app to laravel.

--- resources/js/Pages/Main.jsx
+++ resources/js/Pages/Main.jsx
@@ -1,7 +1,64 @@
+import "./css/app.css";
+import "./css/header.css";
+import "./css/footer.css";
+import "./css/main.css";
+import "./css/post.css";
+import "./css/settings.css"
 
-export default function Welcome() {
+import Posts from "./components/Posts";
+import Settings from "./components/Settings";
+import Header, { initialHeaderState } from "./components/Header";
+import Footer from "./Components/Footer";
+import { useReducer, useEffect, useState } from "react";
+import { headerReducer } from "../../temp/src/state/reducers";
+
+
+// export type stateProps = {
+//   displaySettings: boolean; 
+// }
+
+function Display(state) {
+
+  const displaySettings = state.displaySettings;
+
+  if (displaySettings === undefined || state instanceof Error) {
     return (
-        <><div className="text-white">he;llo</div></>
-            
+    <>Loading...</>
+  );
+
+  } else if (displaySettings === true) {
+    return (
+       <div className="sett-inner">{Settings()}</div>
     );
+
+  } else {
+    return (
+      <div className="main-inner">{Posts()}</div>
+    )
+  }
 }
+
+
+function Main() {
+  const [state, dispatch] = useReducer(headerReducer, initialHeaderState);
+
+  if (state instanceof Error) return (<></>);
+
+  return (
+    <div className="app">
+      <div className="header">
+        <Header dispatch={dispatch}/>
+      </div>
+
+      <div className="main">
+        <Display {...state}/>
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
