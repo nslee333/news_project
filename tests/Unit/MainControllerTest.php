7fdf79c9ebf6e2087488aca9cc560d64d58b3f26
Nathan <nslee333@gmail.com>
2023-05-12 16:26:28 -0700

    Init unit tests

---
+++ tests/Unit/MainControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers;
+use PHPUnit\Framework\TestCase;
+
+class MainControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
