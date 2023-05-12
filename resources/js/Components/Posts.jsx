dde6bd7b4ec0b5db70e288361a7e17412d420dda
Nathan <nslee333@gmail.com>
2023-05-12 12:33:45 -0700

    Added display handling for no response.

--- resources/js/Components/Posts.jsx
+++ resources/js/Components/Posts.jsx
@@ -1,25 +1,31 @@
 
-export default function Posts({ props }) {
 
-  const articles = props.articles;
-  const displayPosts = articles.map((item, index) => {
-    return (
-      <div className="post" key={index}>
-        <a href={articles[index].url} className="post-link">
-          <img
-            className="post-img"
-            src={articles[index].urlToImage}
-            alt="post photo"
-          />
-          <div className="post-info">
-            <div className="post-title">{articles[index].title}</div>
-            <div className="post-author">{articles[index].author}</div>
-            <div className="post-source">{articles[index].source.name}</div>
-          </div>
-        </a>
-      </div>
-    );
-  });
+export default function Posts({props}) {
+  
+  if (props !== undefined) {
+    const articles = props.articles;
+    const displayPosts = articles.map((item, index) => {
+      return (
+        <div className="post" key={index}>
+          <a href={articles[index].url} className="post-link">
+            <img
+              className="post-img"
+              src={articles[index].urlToImage}
+              alt="post photo"
+            />
+            <div className="post-info">
+              <div className="post-title">{articles[index].title}</div>
+              <div className="post-author">{articles[index].author}</div>
+              <div className="post-source">{articles[index].source.name}</div>
+            </div>
+          </a>
+        </div>
+      );
+    });
 
-  return <>{displayPosts}</>
+    return <>{displayPosts}</>
+
+  } else {
+    return <>Error: Please consult your console.</>;
+  }
 }
