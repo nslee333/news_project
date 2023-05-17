2ff696295a682bf2f3c93f3c3fb1f3b02d28e338
Nathan <nslee333@gmail.com>
2023-05-16 21:26:33 -0700

    WIP: Getting test to work.

--- tests/Unit/MainControllerTest.php
+++ tests/Unit/MainControllerTest.php
@@ -8,38 +8,42 @@ use Illuminate\Testing\Fluent\AssertableJson;
 use Illuminate\Testing\TestResponse; 
 
 
-
 class MainControllerTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    // public function test_returns_response_from_api() // 
-    // {
+    public function test_returns_response_from_api() // 
+    {
         
-    //     $controller = new MainController;
+        $controller = new MainController;
 
-    //     $controller->start_cooldown();
+        $controller->start_cooldown();
 
-    //     $decide = $controller->fetch_from_api();
+        $decide = $controller->fetch_from_api();
 
-    //     // $decide
-    //     //     ->assertJson(fn (AssertableJson $json) => 
-    //     //     $json->hasAll(['status', 'totalResults', 'articles'])
-    //     // );
-    //     // $data = $this->getOriginalContent()->getData();
-    //     // dd(json_decode($decide->getBody()));
-    //     $response = json_decode($decide->getBody());
+        $response = json_decode($decide->getBody());
 
-    //     $response
-    //         ->assertJson(fn (AssertableJson $json) => 
-    //             $json->hasAll([
-    //                 'status',
-    //                 'totalResults',
-    //                 'articles',
-    //             ])
-    //         );
+        dd($response);
+        // dd(gettype($response));
 
-    //     // return $decide;
-    // }
+        // $response->assertContent("status");
+
+        // & I don't think I'm using it correctly.
+        // & assertContent is not a method because $response is not an http response.
+        // & Response is an object not a response, that's why the methods don't work.
+        
+        
+
+        // $response
+        //     ->assertJson(fn (AssertableJson $json) => 
+        //         $json->hasAll([
+        //             'status',
+        //             'totalResults',
+        //             'articles',
+        //         ])->etc()
+        //     );
+
+        // return $decide;
+    }
 }
