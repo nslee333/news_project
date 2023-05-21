5d03a708a72773c7cedf9a58ac4e50f88b234b77
Nathan <nslee333@gmail.com>
2023-05-20 20:39:50 -0700

    WIP:

--- tests/Unit/MainControllerTest.php
+++ tests/Unit/MainControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Http\Controllers\MainController;
+use Inertia\Testing\AssertableInertia as Assert;
 
 class MainControllerTest extends TestCase
 {
@@ -11,5 +12,12 @@ class MainControllerTest extends TestCase
      * A basic unit test example.
      */
     
+    public function test_(): void
+    {
+        $controller = new MainController;
+
+        $response = $controller->index();
+
+    }
     
 } 
