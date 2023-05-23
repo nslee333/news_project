6698bed4be94f62ac55991a99278bf57279a8c29
Nathan <nslee333@gmail.com>
2023-05-22 18:23:24 -0700

    Clearing out old code.

--- tests/Feature/MainControllerTest.php
+++ tests/Feature/MainControllerTest.php
@@ -1,83 +1,12 @@
 <?php
 
-// namespace Tests\Unit;
+namespace Tests\Feature;
 
-// use Tests\TestCase;
-// use App\Http\Controllers\MainController;;
+use Tests\TestCase;
+use App\Http\Controllers\MainController;;
 
-// class MainControllerTest extends TestCase
-// {
-//     /**
-//      * A basic unit test example.
-//      */
-    
-//     public function test_response_from_fetch_returns_status_ok() // 
-//     {
-//         $controller = new MainController;
-
-//         $controller->start_cooldown();
-
-//         $decide = $controller->fetch_from_api();
-
-//         $response = json_decode($decide);
-
-//         $this->assertTrue($response->{"status"} === "ok");
-
-//     }
-//     public function test_fetch_response_has_articles() // 
-//     {
-//         $controller = new MainController;
-
-//         $controller->start_cooldown();
-
-//         $decide = $controller->fetch_from_api();
-
-//         $response = json_decode($decide);
-
-//         $articles = $response->{"articles"};
-
-//         $this->assertTrue(gettype($articles) !== "Undefined");
-//     }
-//     public function test_fetch_response_articles_is_an_array() // 
-//     {
-//         $controller = new MainController;
-
-//         $controller->start_cooldown();
-
-//         $decide = $controller->fetch_from_api();
-
-//         $response = json_decode($decide);
-
-//         $articles = $response->{"articles"};
-
-//         $this->assertTrue(gettype($articles) === "array");
-//     }
-//     public function test_fetch_response_article_has_an_author() // 
-//     {
-//         $controller = new MainController;
-
-//         $controller->start_cooldown();
-
-//         $decide = $controller->fetch_from_api();
-
-//         $response = json_decode($decide);
-
-//         $articles = $response->{"articles"};
-
-//         $this->assertTrue(gettype($articles[0]->{"author"}) !== "undefined");
-//     }
-//     public function test_fetch_response_article_has_a_title() // 
-//     {
-//         $controller = new MainController;
-
-//         $controller->start_cooldown();
-
-//         $decide = $controller->fetch_from_api();
-
-//         $response = json_decode($decide);
-
-//         $articles = $response->{"articles"};
-
-//         $this->assertTrue(gettype($articles[0]->{"title"}) !== "undefined");
-//     }
-// } 
+class MainControllerTest extends TestCase
+{
+  // * Test cooldown feature here.
+ 
+} 
