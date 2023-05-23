1595c410e733bf1b20b991a1b3c3deb7cb240bd2
Nathan <nslee333@gmail.com>
2023-05-22 19:27:19 -0700

    WIP: Feature test

--- tests/Feature/MainControllerTest.php
+++ tests/Feature/MainControllerTest.php
@@ -3,10 +3,25 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Http\Controllers\MainController;;
+use App\Http\Controllers\MainController;
 
 class MainControllerTest extends TestCase
 {
   // * Test cooldown feature here.
+
+  public function test_controller_returns_copy_of_data_after_second_request(): void
+  {
+    $controller = new MainController;
+
+    $first_request = $this->get('/');
+
+    $first_check_cooldown = $controller->check_cooldown();
+
+    $this->assertFalse($first_check_cooldown);
+
+    $first_request = $this->get('/');
+
+    // & Stopped at adding second assertion.
+  }
  
 } 
