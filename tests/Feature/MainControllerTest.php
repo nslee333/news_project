81a925a963a7f5b0a05a8b407dd29d34a6a5f55b
Nathan <nslee333@gmail.com>
2023-05-24 15:07:46 -0700

    WIP: Saving progress for later.

--- tests/Feature/MainControllerTest.php
+++ tests/Feature/MainControllerTest.php
@@ -4,34 +4,48 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Artisan;
 
 class MainControllerTest extends TestCase
 {
   // * Test cooldown feature here.
 
-  public function test_controller_returns_copy_of_data_after_second_request(): void
-  {
-    $controller = new MainController;
 
-    $result = $this->get('/');
 
-    dd($controller->cooldown);
+  // public function test_setup(): void
+  // {
+  //   Artisan::call('serve');
 
-    $first_check_cooldown = $controller->check_cooldown();
+  //   Artisan::call("shutdown");
+  //   // * Testing how to setup and teardown application to test
+
+  //   Artisan::call("Ctrl + C");
+  // }
+
+  // public function test_controller_returns_copy_of_data_after_second_request(): void
+  // {
+  //   $controller = new MainController;
+
+  //   $result = $this->get('/');
+
+  //   dd($controller->cooldown);
+
+  //   $first_check_cooldown = $controller->check_cooldown();
     
-    $this->assertFalse($first_check_cooldown);
+  //   $this->assertFalse($first_check_cooldown);
     
-    $this->get('/');
+  //   $this->get('/');
     
-    $second_check_cooldown = $controller->check_cooldown();
+  //   $second_check_cooldown = $controller->check_cooldown();
     
-    dd([$first_check_cooldown, $second_check_cooldown]);
+  //   dd([$first_check_cooldown, $second_check_cooldown]);
     
-    // $this->assertTrue($second_check_cooldown);
+  //   // $this->assertTrue($second_check_cooldown);
 
     // & Stopped at adding second assertion.
     // * I don't think this test will work since I need the framework to be running to test this feature - might 
     // * Need to look into phpunit fixtures.
-  }
+    // * I'm going to come back to this later.
+  // }
  
 } 
