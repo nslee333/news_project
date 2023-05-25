05cc7d31a32e889d26d7a279782ddb9389f3fa84
Nathan <nslee333@gmail.com>
2023-05-25 04:37:39 -0700

    Progress save - implementing button

--- resources/js/Components/Settings.jsx
+++ resources/js/Components/Settings.jsx
@@ -2,6 +2,8 @@ import "../../css/settings.css";
 import { settingsReducer } from "../state/reducers";
 import { useReducer } from "react";
 
+// & Need to implement this
+
 
 
 export default function Settings() {
@@ -19,13 +21,13 @@ export default function Settings() {
       return (
         <div className="filter-map" key={index}>
         {state.sourcesWanted[index]}
-        <button className="filter-btn" onClick={() => dispatch(
-          {
-            type: "remove_from_sources_wanted",
-            source: state.sourcesWanted[index]
-          })}
-        >Delete</button>
-      </div>
+          <button className="filter-btn" onClick={() => dispatch(
+            {
+              type: "remove_from_sources_wanted",
+              source: state.sourcesWanted[index]
+            })}
+          >Delete</button>
+        </div>
       )
     });
 
@@ -43,12 +45,12 @@ export default function Settings() {
       return (
         <div className="block-map" key={index}>
           {state.sourcesBlocked[index]}
-        <button className="block-btn" onClick={() => dispatch(
-          {
-            type: "remove_from_blocked_sources",
-            source: state.sourcesBlocked[index]
-          }
-        )}>Delete</button>
+          <button className="block-btn" onClick={() => dispatch(
+            {
+              type: "remove_from_blocked_sources",
+              source: state.sourcesBlocked[index]
+            }
+          )}>Delete</button>
         </div>
       );
     })
@@ -73,15 +75,17 @@ export default function Settings() {
           <div className="feed-title">Customize your feed</div>
           <div className="sett-filter">
             <div className="filter-title">Wanted news sources</div>
-            <div className="filter-title">
+            <div className="filter-div">
               <div className="filter-sources">
                 {
                   state ? displayWantedSources() : <></>
                 }
               </div>
-              <input className="" type="text"/>
+              <div className="filter-input">
+                <input className="" type="text"/>
+                <button className="filter-save">Save</button>
+              </div>
             </div>
-            
           </div>
 
           <div className="sett-block">
@@ -91,10 +95,12 @@ export default function Settings() {
                   state ? displaySourcesBlocked() : <></>
                 }
               </div>
-              <input type="text" />
+              <div className="block-input">
+                <input type="text" />
+                <button className="block-save">Save</button>
+              </div>
           </div>
         </div>
-
       </div>
 
     </> 
